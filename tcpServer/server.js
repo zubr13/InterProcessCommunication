a0a8d1559335c4b1715905a0c47e02e09988e32f
Andrey Zubritskiy <andreizubritskiy@gmail.com>
2016-05-15 16:08:24 +0300

    Task 3 is completed

--- tcpServer/server.js
+++ tcpServer/server.js
@@ -4,39 +4,27 @@ api.net = require('net');
 api.cluster = require('cluster');
 api.os = require('os');
 
-var initialArr = [3, 5, -3, 5.3, 20,  -9, 43, -23.7, 65, 76, 34, 56];
+var initialArr = [3, 5, -3, 5.3, 20,  -9, 43, -23.7];
 var results = [];
 var cpuCount = api.os.cpus().length;
 var clients = [];
+var index = {};
 
 var server = api.net.createServer(function(socket) {
   clients.push(socket);
+  index[socket] = clients.length;
   console.log('Connected: ' + socket.localAddress);
-  console.log("Cpu count: " + cpuCount);
+  console.log("Cpu count: " + cpuCount)
   console.log("The number of clients: " + clients.length);
 
-  var taskNumber;
-  var start = 0;
-  var end;
-  results = [];
-  for(var i = 0; i < clients.length; i++) {
-    taskNumber = initialArr.length / clients.length;
-    end = start + taskNumber;
-    if(i === clients.length - 1 && end < initialArr.length){
-      end = initialArr.length;
-    }
-    console.log("Start: " + start);
-    console.log("End: " + end);
-    clients[i].write(JSON.stringify(initialArr.slice(start, end)));
-    start = end;
-  }
+  var start = initialArr.length / cpuCount * (clients.length - 1);
+  var end = start + initialArr.length / cpuCount;
 
+  var slicedArr = JSON.stringify(initialArr.slice(start, end));
+  socket.write(slicedArr);
   socket.on('data', function(data) {
     console.log('Data received (by server): ' + data);
-    var result = JSON.parse(data);
-    result.forEach(function(item, i, arr){
-      results.push(item);
-    });
+    results[index[socket]] = JSON.parse(data);
     console.log("Results: " + results);
   });
 }).listen(2000);
